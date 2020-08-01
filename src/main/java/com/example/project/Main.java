2af07760b53876a74d5abb5ca8239da837072499
CSBlues <laforests@ashevilleschool.org>
2020-08-01 19:09:59 -0400

    submission 1

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -4,9 +4,31 @@ import java.util.Scanner;
 
 public class Main
 {
+
+
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+        // Choose a random number from 0-100
+
+        int rand = (int)(Math.random() * 100) + 1;
+        System.out.println("Guess an integer between 1-100.");
+        int guess = scan.nextInt();
+
+        while (guess != rand){
+            if (guess < rand){
+                System.out.println("Too low!");
+                System.out.println("Guess an integer between 1-100.");
+                guess = scan.nextInt();
+            }else if (guess > rand){
+                System.out.println("Too high!");
+                System.out.println("Guess an integer between 1-100.");
+                guess = scan.nextInt();
+            }else {
+                System.out.println("You got it!");
+            }
+        }
+        System.out.println("You got it!");
 
         // Ask the user to guess a number from 0 to 100
         // Get the first guess using scan.nextInt();
@@ -20,8 +42,10 @@ public class Main
 
         // Get a new guess (save it into the same variable)
 
-        // Print out "You got it!"
+
+        // Print out something like "You got it!"
 
 
     }
+
 }
